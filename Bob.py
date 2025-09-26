db7205d2e1e0c8faff0d6d713bfb6546b4ec5ad4
Warsut <kaylin367@gmail.com>
2025-09-26 15:08:44 -0400

    Started working on basic styling.
    Changed icons (added them to assets folder).
    Injected styling, added css.
    Added button and side column, started styling with coloring for them.

--- Bob.py
+++ Bob.py
@@ -1,10 +1,24 @@
 import streamlit as st
 import ollama
 
+#function to load the css styling
+def load_css(file_path):
+    with open(file_path) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+#call the function to load the styling
+load_css("Styling/style.css") 
+
 
 MODEL = 'llama3.1:8b' #this is the model we are using
 
 st.title("Hi, I'm Bob!")
+
+st.sidebar.title("BOB A.I.")
+with st.sidebar:
+    st.markdown("What does this do?")
+    st.button("+ New Chat")
+
 
 #initializes the messages
 if 'messages' not in st.session_state:
@@ -14,6 +28,7 @@ if 'messages' not in st.session_state:
 for message in st.session_state["messages"]:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+
 
 
 
@@ -28,10 +43,10 @@ def generate_response():
 
 if prompt:= st.chat_input("Type here"): #this text will show up in the input bar
     st.session_state.messages.append({"role": "user", "content": prompt}) #if the user types a prompt append it
-    with st.chat_message("user"):
+    with st.chat_message("user", avatar="Assets/User_Icon.png"):
         st.markdown(prompt) #display prompt
     st.session_state['full_message'] = "" #defines a session state for the full message, empty at first as no request has yet been made
-    with st.chat_message("assistant"):
+    with st.chat_message("assistant", avatar="Assets/smiley.jpg"):
         stream = generate_response()
         response = st.write_stream(stream) #write the stream response
         st.session_state.messages.append({'role': 'assistant', 'content': response}) #append assitant response into content
