fcd533bcdefa8caee21da67c7d94dbc2ccb5c476
Warsut <kaylin367@gmail.com>
2025-11-14 10:49:32 -0500

    In progress

--- run_Bob.py
+++ run_Bob.py
@@ -1,4 +1,6 @@
 from streamlit.web import cli
 
 if __name__ == "__main__":
-    cli._main_run_clExplicit('Bob.py', 'streamlit run')
+    #cli._main_run_clExplicit('Bob.py', 'streamlit run')
+    #cli._main_run_clExplicit("streamlit", "run", "Bob.py", "--server.port", "8501")
+    cli._main_run_clExplicit("Bob.py", "streamlit run", "--server.port", "3000")
